e9d132c9447fa22d221a28afae6313bf49f7dd33
sanja <sanja.mandic@golive.com>
2014-09-15 10:51:09 +0200

    Initial commit

---
+++ application/Bootstrap.php
@@ -0,0 +1,18 @@
+<?php
+
+class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
+{
+
+    /**
+     * init database adapter
+     */
+    protected function _initDatabase ()
+    {
+        $resource = $this->getPluginResource('multidb');
+        $resource->init();
+
+        Zend_Registry::set('fun_vas', $resource->getDb('fun_vas'));
+        Zend_Registry::set('fun_bulk', $resource->getDb('fun_bulk'));
+    }
+}
+
