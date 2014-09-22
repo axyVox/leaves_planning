acd8c2788c83835379591026136f8d59c5796ada
Branka Stanic <branka.stanic@golive.rs>
2014-09-22 15:37:02 +0200

    Localization

--- application/controllers/IndexController.php
+++ application/controllers/IndexController.php
@@ -1,17 +1,18 @@
 <?php
 
-class IndexController extends Zend_Controller_Action
+class IndexController extends Bi_BaseController
 {
 
     public function init()
     {
         /* Initialize action controller here */
+        parent::init();
     }
 
     public function indexAction()
     {
-
-
+        $this->view->translate = $this->translate;
+        $this->view->sess_lang = $this->session->lang;
     }
 
     public function testAction()
@@ -22,6 +23,20 @@ class IndexController extends Zend_Controller_Action
         $this->view->mess = $request->getParam('mess','get_is_not_set');
     }
 
+    public function setLanguageAction(){
+
+        $lang = $this->getRequest()->getParam('lang');
+
+//        $this->view->layout()->disableLayout();
+//        $this->getHelper('viewRenderer')->setNoRender();
+
+        $this->session->lang = $lang;
+
+//        echo '<br>';
+//        echo 'session: '.$this->session->lang;
+
+        $this->redirect('/index');
+    }
 
 }
 
